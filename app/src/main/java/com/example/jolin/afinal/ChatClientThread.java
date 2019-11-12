d830dcf7939c4e44a85fb963629c4e8fd2d162f0
00557149 <00557149@mail.ntou.edu.tw>
2019-11-12 15:08:54 +0800

    socket

--- app/src/main/java/com/example/jolin/afinal/ChatClientThread.java
+++ app/src/main/java/com/example/jolin/afinal/ChatClientThread.java
@@ -1,6 +1,5 @@
 package com.example.jolin.afinal;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,10 +10,8 @@ public class ChatClientThread extends Thread {
 
     private static Socket socket = null;
     private static Client client = null;
-    // private static DataInputStream dis = null;
     private static OutputStream os = null;
     private static InputStream is = null;
-    private ByteArrayOutputStream baos = null;
     private FileOutputStream fos = null;
     private int readLength = 0;
     private static byte[] byteArray;
@@ -29,27 +26,19 @@ public class ChatClientThread extends Thread {
     }
 
     public void open() {
-        os = null;
-        is = null;
-        /*try {
-            // dis = new DataInputStream(socket.getInputStream());
-            // os = socket.getOutputStream();
-            // is = socket.getInputStream();
-            os = null;
-            is = null;
-            // baos = new ByteArrayOutputStream();
+        try {
+            os = socket.getOutputStream();
+            is = socket.getInputStream();
         } catch (IOException e) {
             System.out.println("Error getting input stream : " + e.getMessage());
             client.stop();
-        }*/
+        }
     }
 
     public void close() {
         try {
-            // dis.close();
             is.close();
-            // os.close();
-            // baos.close();
+            os.close();
         } catch (IOException e) {
             System.out.println("Error closing input stream : " + e.getMessage());
         }
@@ -64,89 +53,30 @@ public class ChatClientThread extends Thread {
         System.out.print("----------------ChatClientThread----------------");
         /*從Server傳入影像byte，再輸出成file*/
         try {
-            is = socket.getInputStream();
             System.out.print(StartGameActivity.imageFileReceivePath);
-            os = new FileOutputStream(StartGameActivity.imageFileReceivePath); //Gets the true path of your image
+            // os = new FileOutputStream(StartGameActivity.imageFileReceivePath); //Gets the true path of your image
+            // os = new FileOutputStream("C:\\Users\\User\\Desktop\\test\\receive.jpg"); //Gets the true path of your image
+            fos = new FileOutputStream(StartGameActivity.imageFileReceivePath); //Gets the true path of your image
+
             System.out.println("--------------333333333333333333----------------");
             while ((readLength = is.read()) != -1) {
-                os.write(readLength); //Writes bytes to output stream
-                System.out.println("--------------4444444444444444444----------------");
+                fos.write(readLength); //Writes bytes to output stream
+                System.out.println(readLength);
+                System.out.println("--------------RRRRRRRRRRRRRRRRRRRRRRRRR----------------");
             }
+            // 送多少byte先說，收完要停
+            // 讀一個整數，否則就須把很多byte合起來得到整數
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("--------------777777777777777777777----------------");
             client.stop();
         }
-
-
-        /*try {
-            is = socket.getInputStream();
-            File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES); //Gets information about a said directory on your device - currently downloads
-            File photoPath = new File(StartGameActivity.imageFileReceivePath); //Define your image name I used png but other formats should also work - make sure to specify file extension on server
-            OutputStream output = new FileOutputStream(photoPath.getAbsolutePath()); //Gets the true path of your image
-            // os = new FileOutputStream("C:\\Users\\User\\Desktop\\test\\receive.jpg"); //Gets the true path of your image
-            // os = new FileOutputStream(StartGameActivity.imageFileReceivePath); //Gets the true path of your image
-            System.out.println("--------------333333333333333333----------------");
-            while ((readLength = is.read()) != -1) {
-                output.write(readLength); //Writes bytes to output stream
-                // os.write(readLength); //Writes bytes to output stream
-                System.out.println("--------------4444444444444444444----------------");
-            }*/
-            /*try {
-                try {
-                    while ((readLength = is.read()) != -1) {
-                        os.write(readLength); //Writes bytes to output stream
-                        System.out.println("--------------4444444444444444444----------------");
-                    }
-                } finally {
-                    System.out.println("--------------5555555555555555555555----------------");
-                    //Flushes and closes socket
-                    os.flush();
-                    os.close();
-                }
-            } finally {
-                System.out.println("--------------6666666666666666666666----------------");
-                is.close();
-            }*/
-        /*} catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("--------------777777777777777777777----------------");
-            client.stop();
-        }*/
-
-        // try {
-            /*while (true) {
-                client.handleMessage(dis.readUTF());
-            }*/
-
-            /*fos = new FileOutputStream(StartGameActivity.imageFileReceivePath);
-            while (true) {
-                while((readLength = is.read()) != -1){
-                    fos.write(buffer, 0, readLength);
-                }
-                fos.flush();
-            }*/
-
-            /*while (true){
-                while((readLength = is.read(buffer)) != -1){
-                    baos.write(buffer, 0, readLength);
-                }
-                System.out.println("8888888888888888888888888888888");
-                byteArray = baos.toByteArray();
-                System.out.println(byteArray);
-                createFile(StartGameActivity.imageFileReceivePath, byteArray);
-            }*/
-
-        // } catch (IOException e) {
-            // client.stop();
-        // }
-
     }
 
     //將byte陣列寫入檔案
-    public void createFile(String path, byte[] content) throws IOException {
+    /*public void createFile(String path, byte[] content) throws IOException {
         FileOutputStream fos = new FileOutputStream(path);
         fos.write(content);
         fos.close();
-    }
+    }*/
 }
--- app/src/main/java/com/example/jolin/afinal/Client.java
+++ app/src/main/java/com/example/jolin/afinal/Client.java
@@ -16,12 +16,9 @@ public class Client implements Runnable {
     private int serverPort = 5002;
     private Socket socket = null;
     private Thread thread = null;
-    // private DataInputStream dis = null;
-    // private DataOutputStream dos = null;
     private OutputStream os = null;
     private InputStream is = null;
-    // private ByteArrayInputStream bais = null;
-    // private ByteArrayOutputStream baos = null;
+    private FileInputStream fis = null;
     private ChatClientThread client = null;
     private int readLength = 0;
     private byte[] buffer;
@@ -34,14 +31,10 @@ public class Client implements Runnable {
             System.out.println("Connected to server " + socket.getRemoteSocketAddress());
 
             // test();
-            // dis = new DataInputStream(System.in);
-            // dos = new DataOutputStream(socket.getOutputStream());
-            // os = socket.getOutputStream();
-            os = null;
-            // is = socket.getInputStream();
-            is = null;
-            // bais = null;
-            // baos = new ByteArrayOutputStream();
+            os = socket.getOutputStream();
+            // os = null;
+            is = socket.getInputStream();
+            // is = null;
             buffer = null;
             System.out.println("---------------1111111111111111----------------");
             client = new ChatClientThread(this, socket);
@@ -57,57 +50,30 @@ public class Client implements Runnable {
         System.out.print("----------------ClientThread----------------");
         while (thread != null) {
             System.out.print("Transfer image to server...");
-            // dos.writeUTF(dis.readLine());
-            // dos.flush();
-            /*while(setByteArray()){
-                System.out.print("mmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
-                baos.write(getByteArray(), 0, getByteArray().length);
-            }*/
 
             /*將影像byte讀入，再傳出到Server端*/
             try {
-                os = socket.getOutputStream();
-                is = new FileInputStream(StartGameActivity.imageFilePath); //Gets the true path of your image
+                // os = socket.getOutputStream();
+                // is = new FileInputStream(StartGameActivity.imageFilePath); //Gets the true path of your image
+                fis = new FileInputStream(StartGameActivity.imageFilePath);
                 System.out.println("--------------888888888888888888888----------------");
-                while ((readLength = is.read()) != -1) {
+                while ((readLength = fis.read()) != -1) {
                     os.write(readLength); //Writes bytes to output stream
                     System.out.println("--------------999999999999999999999----------------");
                 }
-                /*try {
-                    try {
-                        while ((readLength = is.read()) != -1) {
-                            os.write(readLength); //Writes bytes to output stream
-                            System.out.println("--------------999999999999999999999----------------");
-                        }
-                    } finally {
-                        System.out.println("--------------000000000000000000----------------");
-                        //Flushes and closes socket
-                        os.flush();
-                        os.close();
-                    }
-                } finally {
-                    System.out.println("--------------zzzzzzzzzzzzzzzzzzz----------------");
-                    is.close();
-                }*/
+                os.flush();
+
+                // Sleep, because this thread must wait ChatClientThread to show the message first
+                try {
+                    thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.println("Error : " + e.getMessage());
+                }
+
             } catch (IOException e) {
                 e.printStackTrace();
                 System.out.println("--------------yyyyyyyyyyyyyyyyyyyyyyyy----------------");
-            }
-
-            /*setByteArray();
-            bais = new ByteArrayInputStream(getByteArray());
-            System.out.println("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy");
-            while((readLength = bais.read(buffer, 0, getByteArray().length))!= -1){
-                System.out.print("mmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
-                baos.write(buffer, 0, readLength);
-            }
-            System.out.print("444444444444444444444444444444444444");*/
-
-            // Sleep, because this thread must wait ChatClientThread to show the message first
-            try {
-                thread.sleep(500);
-            } catch (InterruptedException e) {
-                System.out.println("Error : " + e.getMessage());
+                stop();
             }
         }
     }
@@ -140,12 +106,9 @@ public class Client implements Runnable {
     public void stop() {
         try {
             thread = null;
-            // dis.close();
-            // dos.close();
             os.close();
             is.close();
-            // bais.close();
-            // baos.close();
+            fis.close();
             socket.close();
         } catch (IOException e) {
             System.out.println("Error closing : " + e.getMessage());
